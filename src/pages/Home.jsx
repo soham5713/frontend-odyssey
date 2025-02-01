f89c437140cba5dd3bc4e106a786db6f4312a664
bhosalesooham5713@gmail.com <bhosalesoham5713@gmail.com>
2025-02-01 16:02:09 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,25 +32,25 @@ const topRestaurants = [
     name: "Spice Paradise",
     rating: 4.5,
     cuisine: "North Indian",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/16/99/0d/68/photo1jpg.jpg",
+    image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
   },
   {
     name: "Tandoori Nights",
     rating: 4.2,
     cuisine: "Mughlai",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/10/73/a8/53/tandoori-nights.jpg",
+    image: "https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
   },
   {
     name: "Curry House",
     rating: 4.3,
     cuisine: "South Indian",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/23/dd/23/e3/curry-house.jpg",
+    image: "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
   },
   {
     name: "Dosa Delight",
     rating: 4.1,
     cuisine: "South Indian",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/11/e9/0c/8d/dosa-delight.jpg",
+    image: "https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
   },
 ]
 
